3dc31c25de8ac6cc0dea48f03702cf349dfb9e03
totalchest <13sergey78@mail.ru>
2020-03-03 00:05:45 +0300

    Update ant file

---
+++ db/Drop_DB.sql
@@ -0,0 +1,2 @@
+DROP TYPE IF EXISTS customer_type, operation_type, account_type CASCADE;
+DROP TABLE IF EXISTS departments, accounts, operations, types_of_account, customers, contacts CASCADE;
---
+++ db/Select_DB.sql
@@ -0,0 +1,6 @@
+SELECT * FROM departments;
+SELECT * FROM types_of_account;
+SELECT * FROM customers;
+SELECT * FROM contacts;
+SELECT * FROM accounts;
+SELECT * FROM operations;
