dbf0fc7cb400418073b9d6113547f63184e0dc1c
totalchest <13sergey78@mail.ru>
2020-03-27 13:54:26 +0300

    Tests done.

--- src/org/Main/Main.java
+++ src/org/Main/Main.java
@@ -1,33 +1,6 @@
 package org.Main;
 
-import org.db.DAOFactory;
-import org.db.dao.*;
-import org.db.model.*;
-
-import java.sql.SQLException;
-import java.util.Properties;
-
 public class Main {
-    public static void main(String[] args)  throws SQLException {
-        DAOFactory factory = DAOFactory.getInstance();
-        if(factory != null){
-            System.out.println("DAOFactory done.");
-            AccountDAO accountDAO = factory.getAccountDAO();
-            Account a = (Account) accountDAO.getAccountById(8);
-            System.out.println(accountDAO.getAllAccounts().size());
-            accountDAO.deleteAccount(a);
-            System.out.println(accountDAO.getAllAccounts().size());
-           // accountDAO.deleteAccount(accountDAO.getAccountById(3));
-            //System.out.println(accountDAO.getAllAccounts().size());
-            /*int id = o.getId();
-            System.out.println(id);
-            int oldSize = operationDAO.getAllOperations().size();
-            operationDAO.deleteOperationById(id);
-            int newSize = operationDAO.getAllOperations().size();
-            System.out.println(operationDAO.getOperationById(id));
-            System.out.println(newSize);
-            //Assert.assertNull(contactDAO.getContactById(id));
-            //Assert.assertEquals(newSize, oldSize - 1);*/
-        }
+    public static void main(String[] args) {
     }
 }
--- test/org/db/DAOTests.java
+++ test/org/db/DAOTests.java
@@ -24,15 +24,15 @@ public class DAOTests {
         int size = accountDAO.getAllAccounts().size();
         Assert.assertEquals(size, 9);
 
-        Assert.assertEquals(accountDAO.getAccountById(1).getNumber(), "9008007001");
+        Assert.assertEquals(accountDAO.getAccountById(2).getNumber(), "9008007002");
 
         Account a = new Account();
         a.setNumber("8008707005");
-        a.setCustomer(customerDAO.getCustomerById(1));
+        a.setCustomer(customerDAO.getCustomerById(2));
         a.setBalance(Float.valueOf(100000));
-        a.setType(typeOfAccountDAO.getTypeOfAccountById(4));
-        a.setInterestAccount(accountDAO.getAccountById(1));
-        a.setDepartment(departmentDAO.getDepartmentById(3));
+        a.setType(typeOfAccountDAO.getTypeOfAccountById(2));
+        a.setInterestAccount(accountDAO.getAccountById(2));
+        a.setDepartment(departmentDAO.getDepartmentById(2));
         a.setDate(Timestamp.valueOf("2020-03-21 06:40:01.0"));
         int id = accountDAO.addAccount(a);
         Assert.assertEquals(size + 1, accountDAO.getAllAccounts().size());
@@ -45,15 +45,23 @@ public class DAOTests {
         accountDAO.deleteAccountById(a.getId());
         Assert.assertEquals(size, accountDAO.getAllAccounts().size());
 
-        accountDAO.deleteAccount(accountDAO.getAccountById(2));
-        Assert.assertEquals(size - 1, accountDAO.getAllAccounts().size());
+        Account b = new Account();
+        b.setNumber("8008707008");
+        b.setCustomer(customerDAO.getCustomerById(2));
+        b.setBalance(Float.valueOf(100000));
+        b.setType(typeOfAccountDAO.getTypeOfAccountById(2));
+        b.setInterestAccount(accountDAO.getAccountById(2));
+        b.setDepartment(departmentDAO.getDepartmentById(2));
+        b.setDate(Timestamp.valueOf("2020-03-23 12:00:20.0"));
+        id = accountDAO.addAccount(b);
+        accountDAO.deleteAccount(accountDAO.getAccountById(id));
+        Assert.assertEquals(size , accountDAO.getAllAccounts().size());
     }
-
-    private static Set<Integer> departmentIdSet = new HashSet<>();
 
     @Test
     public void testGetAll() throws SQLException {
-        for (int i = 1; i <= 5; ++i) {
+        Set<Integer> departmentIdSet = new HashSet<>();
+        for (int i = 1; i <= 100; ++i) {
             Department d = departmentDAO.getDepartmentById(i);
             if (d != null) {
                 departmentIdSet.add(d.getId());
@@ -78,8 +86,6 @@ public class DAOTests {
         Assert.assertEquals(d.getName(), d1.getName());
         Assert.assertEquals(d.getAdress(), d1.getAdress());
         Assert.assertEquals(newSize, oldSize + 1);
-
-        departmentIdSet.add(id);
     }
 
     @Test(expected = SQLException.class)
@@ -102,13 +108,13 @@ public class DAOTests {
 
     @Test
     public void testDeleteById() throws SQLException {
-        Account a = (Account) accountDAO.getAllAccounts().get(6);
-        int id = a.getId();
-        int oldSize = accountDAO.getAllAccounts().size();
-        accountDAO.deleteAccountById(id);
-        int newSize = accountDAO.getAllAccounts().size();
+        Customer c = (Customer) customerDAO.getAllCustomers().get(0);
+        int id = c.getId();
+        int oldSize = customerDAO.getAllCustomers().size();
+        customerDAO.deleteCustomerById(id);
+        int newSize = customerDAO.getAllCustomers().size();
 
-        Assert.assertNull(accountDAO.getAccountById(id));
+        Assert.assertNull(customerDAO.getCustomerById(id));
         Assert.assertEquals(newSize, oldSize - 1);
     }
 
@@ -131,12 +137,12 @@ public class DAOTests {
 
     @Test
     public void testUpdate() throws SQLException {
-        Customer c = customerDAO.getCustomerById(1);
+        Customer c = customerDAO.getCustomerById(2);
         int oldSize = customerDAO.getAllCustomers().size();
         c.setDateOfRegistration(Timestamp.valueOf("2019-07-11 13:00:18.0"));
         customerDAO.updateCustomer(c);
         int newSize = customerDAO.getAllCustomers().size();
-        c = customerDAO.getCustomerById(1);
+        c = customerDAO.getCustomerById(2);
         Timestamp date = c.getDateOfRegistration();
 
         Assert.assertEquals(oldSize, newSize);
@@ -154,20 +160,22 @@ public class DAOTests {
         TypeOfAccount t = new TypeOfAccount();
         typeOfAccountDAO.updateTypeOfAccount(t);
     }
-/*
+
     @Test(expected = SQLException.class)
     public void testUpdateNoRequiredField() throws SQLException {
-        Customer c = customerDAO.getCustomerById(0);
-        c.setType(null);
+        Customer c = customerDAO.getCustomerById(2);
+        c.setName(null);
         customerDAO.updateCustomer(c);
     }
-*/
+
     @Test
     public void testGetById() throws SQLException {
         Account a = accountDAO.getAccountById(6);
 
         Assert.assertEquals(a.getNumber(), "9008007006");
         Assert.assertEquals(a.getCustomer().getId(), Integer.valueOf(3));
+        Assert.assertEquals(a.getType().getId(), Integer.valueOf(3));
+        Assert.assertEquals(a.getDepartment().getId(), Integer.valueOf(3));
         Assert.assertEquals(a.getInterestAccount().getId(), Integer.valueOf(5));
         Assert.assertEquals(a.getBalance(), Float.valueOf(18000));
         Assert.assertEquals(a.getDate(), Timestamp.valueOf("2020-02-05 16:58:45.0"));
@@ -175,126 +183,44 @@ public class DAOTests {
         a = accountDAO.getAccountById(2000000);
         Assert.assertNull(a);
     }
-/*
+
     @Test
-    public void testVacancyMatchers() {
-        Company c1 = new Company();
-        {
-            c1.setId(1);
-            c1.setAddress("1");
-            c1.setName("1");
+    public void testCustomerWithContact() throws SQLException{
+        Customer cus = new Customer();
+        cus.setName("Круглов Алексей");
+        cus.setDateOfRegistration(Timestamp.valueOf("2020-03-27 12:36:33.0"));
+        cus.setType(CustomerType.INDIVIDUAL);
+        int id_cus = customerDAO.addCustomer(cus);
+        Contact con = new Contact();
+        con.setCustomer(customerDAO.getCustomerById(id_cus));
+        con.setName("Алексей");
+        con.setSurname("Круглов");
+        con.setAdress("Ул. Академика Иванова, д.3");
+        con.setPhoneNumber("8 (999) 780 66 54");
+        con.setEmail("krug_alex@ya.ru");
+        int id_con = contactDAO.addContact(con);
+
+        Assert.assertEquals(customerDAO.getCustomerById(id_cus).getName(), "Круглов Алексей");
+        for (Contact entry : customerDAO.getCustomerById(id_cus).getContactSet()) {
+            Assert.assertEquals(entry.getName(), "Алексей");
         }
-        Company c2 = new Company();
-        {
-            c2.setId(2);
-            c2.setAddress("2");
-            c2.setName("2");
-        }
-        Company c3 = new Company();
-        {
-            c3.setId(3);
-            c3.setAddress("3");
-            c3.setName("3");
-        }
-        Vacancy v = new Vacancy();
-        {
-            v.setId(1);
-            v.setMaxSalary(2000);
-            v.setPosition("president");
-            v.setMinSalary(500);
-            v.setMinEducationLevel(EducationLevel.MASTERS);
-            v.setCompany(c1);
-            v.setPrevCompanyReq(c1);
-            v.setPrevPositionReq("governor");
-        }
-        Worker w = new Worker();
-        {
-            w.setId(1);
-            w.setAddress("1");
-            w.setFirstName("Vladimir");
-            w.setLastName("Putin");
-            w.setEducationLevel(EducationLevel.DOCTORATE);
-            w.setLookingForJob(true);
-            w.setMinSalaryReq(1234);
-            w.setPhoneNumber("1");
-            w.setPositionReq("president");
-        }
-        JobHistory h1 = new JobHistory();
-        {
-            h1.setId(1);
-            h1.setWorker(w);
-            h1.setCompany(c3);
-            h1.setPosition("Governor");
-            h1.setSalary(1);
-        }
-        JobHistory h2 = new JobHistory();
-        {
-            h2.setId(2);
-            h2.setWorker(w);
-            h2.setCompany(c1);
-            h2.setPosition("Janitor");
-            h2.setSalary(123);
-        }
-        w.getHistoryEntries().add(h1);
-        w.getHistoryEntries().add(h2);
-
-        Assert.assertTrue(v.matchesWorker(w));
-        Assert.assertEquals(v.matchesWorker(w), w.matchesVacancy(v));
-
-        w.setPositionReq("President");
-        Assert.assertTrue(v.matchesWorker(w));
-
-        v.setPrevCompanyReq(c2);
-        Assert.assertFalse(v.matchesWorker(w));
-        v.setPrevCompanyReq(c1);
-
-        w.setEducationLevel(EducationLevel.LESS);
-        Assert.assertFalse(w.matchesVacancy(v));
-        w.setEducationLevel(EducationLevel.DOCTORATE);
-
-        w.setLookingForJob(false);
-        Assert.assertFalse(v.matchesWorker(w));
-        w.setLookingForJob(true);
-
-        w.getHistoryEntries().remove(h2);
-        Assert.assertFalse(v.matchesWorker(w));
-        w.getHistoryEntries().add(h2);
-
-        v.setMinSalary(null);
-        Assert.assertTrue(v.matchesWorker(w));
-
-        w.setMinSalaryReq(0);
-        Assert.assertTrue(v.matchesWorker(w));
-
-        w.setPositionReq(null);
-        Assert.assertTrue(v.matchesWorker(w));
-
-        v.setPrevCompanyReq(null);
-        Assert.assertTrue(v.matchesWorker(w));
-
-        v.setPrevPositionReq(null);
-        Assert.assertTrue(v.matchesWorker(w));
-
-        w.setMinSalaryReq(10000000);
-        Assert.assertFalse(v.matchesWorker(w));
-        w.setMinSalaryReq(0);
-
-        w.setPositionReq("persident");
-        Assert.assertFalse(v.matchesWorker(w));
-        w.setPositionReq("president");
     }
 
     @Test
     public void testDAOHelpers() throws SQLException {
-        List workers = DAOHelpers.ExecuteInSession(session -> session.createQuery("from Worker").list());
+        List types = DAOHelpers.ExecuteInSession(session -> session.createQuery("from TypeOfAccount").list());
         Set<Integer> ids = new HashSet<>();
         ids.add(1);
         ids.add(2);
         ids.add(3);
         ids.add(4);
         ids.add(5);
-        for (Object w : workers) {
-            Assert.assertTrue(ids.contains(((Worker) w).getId()));
+        ids.add(6);
+        ids.add(7);
+        ids.add(8);
+        ids.add(9);
+        for (Object t : types) {
+            Assert.assertTrue(ids.contains(((TypeOfAccount) t).getId()));
         }
     }
 
@@ -309,17 +235,4 @@ public class DAOTests {
         DAOHelpers.ExecuteInSessionVoidRet(session -> session.getTransaction().rollback());
     }
 
-
-    @Test
-    public void testEnumOperationType() throws SQLException{
-        int credit = 0, debit = 0;
-        Assert.assertEquals(operationDAO.getOperationById(1).getOperation(), OperationType.CREDIT);
-       // for(int i = 1; i < 7; ++i){
-         //   if(operationDAO.getOperationById(i).getOperation().getValue() == 0)
-           //     ++credit;
-           // else
-             //   ++debit;
-        //}
-    }
-*/
 }
